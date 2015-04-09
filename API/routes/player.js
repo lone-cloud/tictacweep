e1e1a5d08bab39fdf3db142010aeeb0291a12d7e
Egor Philippov <ephilippov@absolute.com>
2015-04-09 14:02:17 -0700

    Laying ( some ) ground work for the API

--- API/routes/player.js
+++ API/routes/player.js
@@ -1,9 +1,20 @@
-var router = require('express').Router(),
-    path   = require('path');
+var router       = require('express').Router(),
+    path         = require('path'),
+    playerWorker = require('./workers/playerWorker');
+
+var moveCache = {};
 
 router
     .post('/move', function(req, res){
-        res.json({'action': 'move', index: [0, 1]});
+    	var move;
+
+    	if( req.body.gameBoard in moveCache ){
+    		move = moveCache[req.body.gameBoard]};
+    	} else {
+    		move = playerWorker.getMove(req.body.gameBoard);
+       	}
+
+    	res.json({'action': 'move', 'index': move});
     });
 
 module.exports = router;
---
+++ API/routes/workers/playerWorker.js
@@ -0,0 +1,5 @@
+module.exports = {
+	getMove: {
+		return [0,1];
+	}
+};
